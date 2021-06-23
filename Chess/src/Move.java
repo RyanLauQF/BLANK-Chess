3904c5f37ca044290f75b391a93c888c7b27083c
RyanLauQF <63765620+RyanLauQF@users.noreply.github.com>
2021-06-23 20:26:56 +0800

    Updated Move.java to process Enpassant moves

--- Chess/src/Move.java
+++ Chess/src/Move.java
@@ -23,15 +23,26 @@ public class Move {
         // set the start tile to null
         if(endTile.isOccupied()){   // attacking an enemy piece
             attackedPiece = endTile.getPiece(); // store a copy of attacked piece to undo move afterwards
+            board.killPiece(getEnd());
         }
+        // check if the move is an enpassant capture
+        else if(isEnpassantCapture()){
+            // kill the enemy pawn
+            int enpassantPawn = board.getEnpassantPawnPosition();
+            attackedPiece = board.getTile(enpassantPawn).getPiece();
+            board.killPiece(enpassantPawn);
+        }
+
         if(startTile.getPiece().toString().equals("K")){    // if piece is a King
             // update king position on board
-            board.setKingPosition(getEnd());
+            board.setKingPosition(getEnd(), startTile.getPiece().isWhite());
         }
+
         // set piece on end tile to piece on start tile
         endTile.setPiece(startTile.getPiece());
         // remove piece on start tile
         startTile.setPiece(null);
+        calculateAttackData();
     }
 
     // undo the move made on the board
@@ -41,17 +52,54 @@ public class Move {
 
         if(endTile.getPiece().toString().equals("K")){    // if piece is a King
             // update king position on board
-            board.setKingPosition(getStart());
+            board.setKingPosition(getStart(), endTile.getPiece().isWhite());
         }
 
         // move the piece back from end to start tile
         startTile.setPiece(endTile.getPiece());
 
         if(attackedPiece != null){  // undo the attacking move
-            endTile.setPiece(attackedPiece);
+            board.getTile(attackedPiece.getPosition()).setPiece(attackedPiece);
+            board.addPiece(attackedPiece, attackedPiece.getPosition());
         }
         else{   // remove piece on end tile after it has moved to start tile
             endTile.setPiece(null);
+        }
+        calculateAttackData();
+    }
+
+    /**
+     * Checks move being made is an Enpassant capture
+     * @return true if it is an enpassant move
+     */
+    private boolean isEnpassantCapture(){
+        return board.getTile(getStart()).getPiece().toString().equals("P") && board.getEnpassant() == getEnd();
+    }
+
+    /**
+     * Checks if the king movement is a castling move
+     * @return true if king is making a castling move, else return false
+     */
+    private boolean isCastling(){
+        if(board.isWhiteTurn()){
+            return getStart() == 60 && (getEnd() == 62 || getEnd() == 58);
+        }
+        else{
+            return getStart() == 4 && (getEnd() == 6 || getEnd() == 2);
+        }
+    }
+
+    /**
+     * Resets and calculates the attack maps for opponents after making a move
+     */
+    private void calculateAttackData(){
+        // if white made a move, recalculate black's attack map
+        board.resetAttackData(!board.isWhiteTurn());
+        if(board.isWhiteTurn()){
+            board.calcBlackAttackMap();
+        }
+        else{
+            board.calcWhiteAttackMap();
         }
     }
 
