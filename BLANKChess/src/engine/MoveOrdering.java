5ad73c7505533b53376a489e55fa926426e1e7b8
RyanLauQF <lauquanfengryan@gmail.com>
2021-11-05 15:59:40 +0800

    Reorganised engine files

---
+++ BLANKChess/src/engine/MoveOrdering.java
@@ -0,0 +1,152 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MoveOrdering {
+    private static final int KING_INDEX = 0;
+    private static final int QUEEN_INDEX = 1;
+    private static final int ROOK_INDEX = 2;
+    private static final int BISHOP_INDEX = 3;
+    private static final int KNIGHT_INDEX = 4;
+    private static final int PAWN_INDEX = 5;
+    private static final int NONE_INDEX = 6;
+
+    private static final int[][] MVV_LVA_SCORES = {
+            {0, 0, 0, 0, 0, 0, 0},          // victim K, attacker K, Q, R, B, N, P, None
+            {50, 51, 52, 53, 54, 55, 0},    // victim Q, attacker K, Q, R, B, N, P, None
+            {40, 41, 42, 43, 44, 45, 0},    // victim R, attacker K, Q, R, B, N, P, None
+            {30, 31, 32, 33, 34, 35, 0},    // victim B, attacker K, Q, R, B, N, P, None
+            {20, 21, 22, 23, 24, 25, 0},    // victim N, attacker K, Q, R, B, N, P, None
+            {10, 11, 12, 13, 14, 15, 0},    // victim P, attacker K, Q, R, B, N, P, None
+            {0, 0, 0, 0, 0, 0, 0},          // victim None, attacker K, Q, R, B, N, P, None
+    };
+
+    public static ArrayList<Short> orderMoves(ArrayList<Short> moves, Search searcher, int searchPly) {
+        moves.sort((move1, move2) -> {
+            int moveScore1 = getMoveScore(move1, searcher, searchPly);
+            int moveScore2 = getMoveScore(move2, searcher, searchPly);
+
+            return Integer.compare(moveScore2, moveScore1);
+        });
+        return moves;
+    }
+
+    private static int getMoveScore(Short move, Search searcher, int ply){
+        Board board = searcher.board;
+
+        // evaluate the move scores
+        int score = 0;
+        int start = MoveGenerator.getStart(move);
+        int end = MoveGenerator.getEnd(move);
+
+        if(searcher.pvMoveScoring){
+            if (searcher.PVMoves[0][ply] == move)
+            {
+                // disable score PV flag
+                searcher.pvMoveScoring = false;
+
+                // give PV move the highest score to search it first
+                return 20000;
+            }
+        }
+
+        boolean isWhitePiece = board.getTile(start).getPiece().isWhite();
+        Piece startPiece = board.getTile(start).getPiece();
+
+        if(MoveGenerator.isCapture(move)){
+            // Sort by Most-Valuable Victim / Least-Valuable Aggressor
+            if (MoveGenerator.getMoveType(move) == 4) {
+                // normal capture
+                score += MVV_LVA(board.getTile(end).getPiece(), startPiece);
+            }
+            else{
+                // enpassant capture
+                score += MVV_LVA_SCORES[PAWN_INDEX][PAWN_INDEX];  // pawn (victim) - pawn (attacker) capture
+            }
+            score += 10000; // prioritise captures
+        }
+        // quiet moves positions
+        else{
+            // killer move
+            if(move == searcher.killerMoves[0][ply]){
+                score += 8000;
+            }
+            else if(move == searcher.killerMoves[1][ply]){
+                score += 7000;
+            }
+            else{
+               // history move score
+                score += searcher.historyMoves[start][end];
+            }
+        }
+
+        if(MoveGenerator.isPromotion(move)){
+            int moveType = MoveGenerator.getMoveType(move);
+            if(moveType == 8 || moveType == 12){
+                // knight promotion
+                score += Knight.KNIGHT_MG_VALUE;
+            }
+            else if(moveType == 11 || moveType == 15){
+                // queen promotion
+                score += Queen.QUEEN_MG_VALUE;
+            }
+            else{
+                score += 300; // rook and bishop not as useful as Queen / Knight promotion (knight discovered checks)
+            }
+        }
+
+        if(MoveGenerator.isCastling(move)){
+            score += 100;
+        }
+
+        if(board.checkPawnAttacking(isWhitePiece, end, 0) != 0){
+            // if tile being moved to is attacked by enemy pawn, usually bad.
+            score -= startPiece.getMidGameValue();
+        }
+
+        return score;
+    }
+
+    private static int MVV_LVA(Piece victim, Piece attacker){
+        int victimINDEX = getPieceMVVLVAIndex(victim.getType());
+        int attackINDEX = getPieceMVVLVAIndex(attacker.getType());
+
+        return MVV_LVA_SCORES[victimINDEX][attackINDEX];
+    }
+
+    private static int getPieceMVVLVAIndex(Piece.PieceType pieceType){
+        if(pieceType == Piece.PieceType.KING){
+            return KING_INDEX;
+        }
+        else if(pieceType == Piece.PieceType.QUEEN){
+            return QUEEN_INDEX;
+        }
+        else if(pieceType == Piece.PieceType.ROOK){
+            return ROOK_INDEX;
+        }
+        else if(pieceType == Piece.PieceType.BISHOP){
+            return BISHOP_INDEX;
+        }
+        else if(pieceType == Piece.PieceType.KNIGHT){
+            return KNIGHT_INDEX;
+        }
+        else if(pieceType == Piece.PieceType.PAWN){
+            return PAWN_INDEX;
+        }
+        // if piece is null, return index 6 which represents none values
+        return NONE_INDEX;
+    }
+
+    public static void main(String[] args) throws IOException {
+//        Board board = new Board();
+//        board.init("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+//        TranspositionTable TT = new TranspositionTable();
+//        AI ai = new AI(true, board);
+//        ai.iterativeDS(10, false);
+//        ArrayList<Short> allMoves = orderMoves(board.getAllLegalMoves(), board, TT, ai);
+//
+//        for(Short moves : allMoves){
+//            System.out.print(FENUtilities.convertIndexToRankAndFile(MoveGenerator.getStart(moves)) + "-" + FENUtilities.convertIndexToRankAndFile(MoveGenerator.getEnd(moves)) + " ");
+//            System.out.println("Score: " + getMoveScore(moves, board, TT, ai) + " ");
+//        }
+    }
+}
---
+++ BLANKChess/src/engine/Zobrist.java
@@ -0,0 +1,208 @@
+import java.util.Random;
+
+public class Zobrist {
+    /*
+     * Zobrist hashing is a hash function used to hash a board position of the chess board.
+     * This allows for quick look-ups of the same position from a transposition table
+     *
+     * At the start of the program:
+     *      - For each 64 square, 12 random numbers will be calculated for each of the 6 piece types for the 2 sides
+     *      - 4 numbers for castling rights
+     *      - 8 numbers for enpassant available files
+     *      - 1 number to check which sides turn.
+     *
+     * Total of 64 * 12 + 8 + 4 + 1 = 781 random numbers
+     *
+     * Piece indexing in hashKey array:
+     *      0 - white pawn
+     *      1 - white knight
+     *      2 - white bishop
+     *      3 - white rook
+     *      4 - white queen
+     *      5 - white king
+     *      6 - black pawn
+     *      7 - black knight
+     *      8 - black bishop
+     *      9 - black rook
+     *      10 - black queen
+     *      11 - black king
+     *
+     * Castling rights indexing:
+     *      0 - white king side castle
+     *      1 - white queen side castle
+     *      2 - black king side castle
+     *      3 - black queen side castle
+     *
+     * Enpassant hash indexing:
+     *      0 - a file
+     *      1 - b file
+     *      2 - c file
+     *      3 - d file
+     *      4 - e file
+     *      5 - f file
+     *      6 - g file
+     *      7 - h file
+     */
+
+    public static long[][] pieceKeys;
+    public static long isWhiteTurnHash;
+    public static long[] castlingHash;
+    public static long[] enpassantHash;
+
+
+    static{
+        // generate a random number for each piece type at each square
+        pieceKeys = new long[64][12];
+        castlingHash = new long[4];
+        enpassantHash = new long[8];
+
+        Random rand = new Random();
+
+        // generate piece position hash
+        for(int i = 0; i < 64; i++){
+            for(int j = 0; j < 12; j++){
+                pieceKeys[i][j] = rand.nextLong();
+            }
+        }
+
+        // generate turn hash
+        isWhiteTurnHash = rand.nextLong();
+
+        // generate castling rights hash
+        for(int i = 0; i < 4; i++){
+            castlingHash[i] = rand.nextLong();
+        }
+
+        // generate enpassant hash for each file
+        for(int i = 0; i < 8; i++){
+            enpassantHash[i] = rand.nextLong();
+        }
+    }
+
+    /**
+     * Generates a Zobrist hash which represents the board in its current position
+     * @param board refers to the current state of the board to be hashed
+     * @return the Zobrist hash of the board state
+     */
+    public static long generateHash(Board board){
+        long zobristHash = 0;
+        PieceList whitePieces = board.getWhitePieces();
+        PieceList blackPieces = board.getBlackPieces();
+
+        // get piece position data
+        Piece piece;
+        for(int i = 0; i < whitePieces.getCount(); i++){
+            piece = board.getTile(whitePieces.occupiedTiles[i]).getPiece();
+            zobristHash ^= getKey(piece);
+        }
+
+        for(int i = 0; i < blackPieces.getCount(); i++){
+            piece = board.getTile(blackPieces.occupiedTiles[i]).getPiece();
+            zobristHash ^= getKey(piece);
+        }
+
+        // get turn data
+        if(board.isWhiteTurn()){
+            zobristHash ^= isWhiteTurnHash;
+        }
+
+        // get castling data
+        if(board.getWhiteKingSideCastle()){
+            zobristHash ^= castlingHash[0];
+        }
+
+        if(board.getWhiteQueenSideCastle()){
+            zobristHash ^= castlingHash[1];
+        }
+
+        if(board.getBlackKingSideCastle()){
+            zobristHash ^= castlingHash[2];
+        }
+
+        if(board.getBlackQueenSideCastle()){
+            zobristHash ^= castlingHash[3];
+        }
+
+        // get enpassant data
+        if(board.canEnpassant()){
+            zobristHash ^= enpassantHash[Piece.getCol(board.getEnpassant())];
+        }
+
+        return zobristHash;
+    }
+
+    public static long movePiece(long zobrist, int start, int end, Piece piece){
+        int index = getPieceIndex(piece);
+        zobrist ^= pieceKeys[start][index];
+        zobrist ^= pieceKeys[end][index];
+        return zobrist;
+    }
+
+    public static long update(long zobrist, int position, Piece piece){
+        zobrist ^= pieceKeys[position][getPieceIndex(piece)];
+        return zobrist;
+    }
+
+    public static long getKey(Piece piece){
+        int pieceIndex = getPieceIndex(piece);
+        return pieceKeys[piece.getPosition()][pieceIndex];
+    }
+
+    public static int getPieceIndex(Piece piece){
+        if(piece.isWhite()){
+            if(piece.isPawn()){
+                return 0;
+            }
+            else if(piece.isKnight()){
+                return 1;
+            }
+            else if(piece.isBishop()){
+                return 2;
+            }
+            else if(piece.isRook()){
+                return 3;
+            }
+            else if(piece.isQueen()){
+                return 4;
+            }
+            else if(piece.isKing()){
+                return 5;
+            }
+        }
+        else{
+            if(piece.isPawn()){
+                return 6;
+            }
+            else if(piece.isKnight()){
+                return 7;
+            }
+            else if(piece.isBishop()){
+                return 8;
+            }
+            else if(piece.isRook()){
+                return 9;
+            }
+            else if(piece.isQueen()){
+                return 10;
+            }
+            else if(piece.isKing()){
+                return 11;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.init(FENUtilities.startFEN);
+
+        long start = System.nanoTime();
+        long key = Zobrist.generateHash(board);
+        long finish = System.nanoTime();
+        long timeElapsed = finish - start;
+        float convertTime = (float) timeElapsed;
+
+        System.out.println("Zobrist Key: " + key);
+        System.out.println("Time Taken: " + convertTime + " ns");
+    }
+}
